0d380d5b16457267ea21d3d91c86dc1e70b404b0
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-12-02 03:45:34 +0000

    copied 0html to 1html; updated routes and workig on js file

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+// DOM launched
+$(document).ready(function() {
+  // create empty dictionary
+  const amenityDict = {};
+  // if checkbox is checked add to dictionary
+  $("#checkbox").on("input", function() {
+    if ($(this).is(":checked")) {
+        // Ariel: go back and see if this is missing something
+        // needs to store k,v to dict
+      amenityDict[$(this).attr('id')] = $(this).value();
+    // delete amenity if not checked
+    } else {
+        delete amenityDict[$(this).attr('id')];
+    }
+    // will come back to this, adding variable to show dict
+    const amenityList = Object.values(amenityDict).join(', ');
+  });
+});
