62cfff64467b652de51a1aeeea25699b61efa546
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-12-02 04:06:45 +0000

    Ariel: all py files done if no errors, changed routes

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,12 +1,27 @@
 $(document).ready(function () {
-    // Check if API is live
-    $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
-      if (data.status === 'OK') {
-        $('#api_status').addClass('available');
-      } else {
-        $('#api_status').removeClass('available');
-      }
-    });
-        // Ariel: Put the 1-hbnb.js functions here
+  // Check if API is live
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
   });
-  
+  // Ariel: Put the 1-hbnb.js functions here
+  // Needs to be checked:Ariel
+  const amenityDict = {};
+  // if checkbox is checked add to dictionary
+  $("#checkbox").on("input", function() {
+    if ($(this).is(":checked")) {
+      // Ariel: go back and see if this is missing something
+      // needs to store k,v to dict
+      amenityDict[$(this).attr('id')] = $(this).value();
+    // delete amenity if not checked
+    } else {
+        delete amenityDict[$(this).attr('id')];
+    }
+    // will come back to this, adding variable to show dict
+    const amenityList = Object.values(amenityDict).join(', ');
+    console.log(amenityList);
+  });
+});
